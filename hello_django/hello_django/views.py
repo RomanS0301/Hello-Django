9c269444f7f2982995b97eb70e277977d3df8cd7
Roman Sidoruk <roman.sidoruk@gmail.com>
2022-12-01 21:11:19 +0300

    урок 53

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -6,11 +6,11 @@ def about(request):
     return render(request, 'about.html', {'greating': 'hello'})
 
 
+def reverse(request):
+    user_text = request.GET['username']
+    rev = user_text[::-1]
+    return render(request, 'reverse.html', {'word': rev})
+
+
 def home(request):
     return HttpResponse('This is my home')
-
-
-def reverse(request):
-    user_text = request.Get('usertext')
-    reversed = user_text[::-1]
-    return render(request, 'reverse.html')
